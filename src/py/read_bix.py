47ee63f2a11c445808499cca2c99dff1cef9dcee
Anton Dorozhkin <Doliman100@gmail.com>
2025-09-29 16:54:09 +0700

    Fix DXT1, DXT5A and DXN format mapping
    Set DXT1, DXT5A and DXN texture format mapping to BC1, BC4 and BC5.

--- src/py/read_bix.py
+++ src/py/read_bix.py
@@ -52,17 +52,17 @@ class Bix():
                 blocks = Deswizzler.XGUntileSurfaceToLinearTexture(dumped_image_data, width, height, "DXT5")
                 dds = Bix.wrap_as_dds_dx5_bc3_linear(blocks, width, height)
             elif format == 438305106: # D3DFMT_DXT1
-                dumped_image_data = Bix.flip_byte_order_32bit(dumped_image_data)
+                dumped_image_data = Bix.flip_byte_order_16bit(dumped_image_data)
                 blocks = Deswizzler.XGUntileSurfaceToLinearTexture(dumped_image_data, width, height, "DXT1")
-                dds = Bix.wrap_as_dds_dx5_bc3_linear(blocks, width, height)
+                dds = Bix.wrap_as_dds_dx10_bc(71, blocks, width, height) # DXGI_FORMAT_BC1_UNORM
             elif format == 438305147: # D3DFMT_DXT5A
                 dumped_image_data = Bix.flip_byte_order_16bit(dumped_image_data)
-                blocks = Deswizzler.XGUntileSurfaceToLinearTexture(dumped_image_data, width, height, "DXT5A")
-                dds = Bix.wrap_as_dds_dx5_bc3_linear(blocks, width, height)
+                blocks = Deswizzler.XGUntileSurfaceToLinearTexture(dumped_image_data, width, height, "DXT1")
+                dds = Bix.wrap_as_dds_dx10_bc(80, blocks, width, height) # DXGI_FORMAT_BC4_UNORM
             elif format == 438305137: # D3DFMT_DXN
                 dumped_image_data = Bix.flip_byte_order_16bit(dumped_image_data)
-                blocks = Deswizzler.XGUntileSurfaceToLinearTexture(dumped_image_data, width, height, "DXN")
-                dds = Bix.wrap_as_dds_dx5_bc3_linear(blocks, width, height)
+                blocks = Deswizzler.XGUntileSurfaceToLinearTexture(dumped_image_data, width, height, "DXT5")
+                dds = Bix.wrap_as_dds_dx10_bc(83, blocks, width, height) # DXGI_FORMAT_BC5_UNORM
             else:
                 raise ValueError("Unsupported deswizzling format!")
 
