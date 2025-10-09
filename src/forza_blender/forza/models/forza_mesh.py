67959422b61e4d53bf5b46758e8c9991dd60ad72
austinbaccus <austinbaccus@live.com>
2025-10-09 12:06:43 -0700

    Beginning of shader work.

--- src/forza_blender/forza/models/forza_mesh.py
+++ src/forza_blender/forza/models/forza_mesh.py
@@ -1,15 +1,17 @@
+from forza_blender.forza.pvs.read_pvs import PVSTexture
 from .forza_vertex import ForzaVertex
 
 class ForzaMesh:
     def __init__(self, name: str, material_name: str, indices: list[int], vertices: list[ForzaVertex], transform, model_index = None, textures = None, texture_indices: list[int] = None):
         self.name = name
-        self.material_name = material_name
+        self.material_name = material_name # TODO: is this the shader?
         self.indices = indices
         self.vertices = vertices
         self.transform = transform
         self.model_index = model_index
         self.textures: list[PVSTexture] = textures
         self.texture_indices = texture_indices
+        self.shaders = None
 
     def get_face_count(self):
         return len(self.indices) / 3
---
+++ src/forza_blender/forza/shaders/generate_shaders.py
@@ -0,0 +1,4 @@
+# programmatically generates an approximation of each shader from Forza into Blender material
+
+# DIFF_1.fxobj
+# ...
