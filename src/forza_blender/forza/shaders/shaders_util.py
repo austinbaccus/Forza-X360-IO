ca62323267574a94d2222d716d025b25845b7447
austinbaccus <austinbaccus@live.com>
2025-10-14 19:48:34 -0700

    Shader mapping improvement. CAFF code imported.

--- src/forza_blender/forza/shaders/shaders_util.py
+++ src/forza_blender/forza/shaders/shaders_util.py
@@ -3,29 +3,7 @@ import bpy # type: ignore
 from forza_blender.forza.models.forza_mesh import ForzaMesh
 from forza_blender.forza.textures.texture_util import get_image_from_index
 
-def add_extra_images_to_material(mat, images, start_idx, create_empty_nodes: bool):
-    x = -600
-    y = -300
-    for img in images[start_idx:]:
-        if img is not None or create_empty_nodes:
-            tex_extra = mat.node_tree.nodes.new("ShaderNodeTexImage"); tex_extra.image = img; tex_extra.location = (x, y)
-            y = y - 300
-
-def get_images_from_mesh(forza_mesh: ForzaMesh, path_last_texture_folder):
-    images = []
-    for texture in forza_mesh.textures:
-        texture_img = get_image_from_index(path_last_texture_folder, texture.texture_file_name)
-        if texture_img is not None:
-            images.append(texture_img)
-        else:
-            # append NULL texture
-            addon_dir = Path(__file__).resolve().parent
-            img_path = addon_dir / r"null.png"
-            images.append(bpy.data.images.load(str(img_path), check_existing=True))
-
-    return images
-
-def generate_image_texture_nodes_for_material(forza_mesh: ForzaMesh, path_last_texture_folder, nodes, links):
+def generate_image_texture_nodes_for_material(forza_mesh: ForzaMesh, track_folder_path, nodes, links):
     x = 0
     y = 0
     i = 0
@@ -33,7 +11,7 @@ def generate_image_texture_nodes_for_material(forza_mesh: ForzaMesh, path_last_t
         # get texture
         loaded_texture_image = None
 
-        texture_img = get_image_from_index(path_last_texture_folder, texture.texture_file_name)
+        texture_img = get_image_from_index(track_folder_path, texture.texture_file_name)
         if texture_img is not None:
             loaded_texture_image = texture_img
         else:
