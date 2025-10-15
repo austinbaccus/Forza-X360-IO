decedded6f6fdf8931daff796d77dc82013af9dd
austinbaccus <austinbaccus@live.com>
2025-10-14 19:57:20 -0700

    Removing unused UI texture stuff.

--- src/forza_blender/blender/ui.py
+++ src/forza_blender/blender/ui.py
@@ -11,7 +11,6 @@ class FORZA_PT_main(bpy.types.Panel):
         layout = self.layout
 
         # checkboxes
-        layout.prop(context.scene, "use_pregenerated_textures", text="Use pre-generated textures")
         layout.prop(context.scene, "generate_mats", text="Generate materials")
         layout.prop(context.scene, "generate_lods", text="Generate LODs")
 
@@ -20,10 +19,6 @@ class FORZA_PT_main(bpy.types.Panel):
 
         # forza models
         _draw_track_import_box(context, layout)
-
-        # forza textures
-        if context.scene.use_pregenerated_textures:
-            _draw_textures_import_box(context, layout)
         
         # import track button
         is_import_btn_enabled: bool = True
@@ -34,7 +29,8 @@ class FORZA_PT_main(bpy.types.Panel):
             is_import_btn_enabled = not is_track_folder_null_or_empty and not is_texture_folder_null_or_empty
         else:
             is_import_btn_enabled = not is_track_folder_null_or_empty
-
+            
+        _draw_generate_textures_button(context, layout, is_import_btn_enabled)
         _draw_import_button(context, layout, is_import_btn_enabled)
 
 def _draw_forza_game_picker(context, layout):
@@ -83,15 +79,11 @@ def _draw_track_import_box(context, layout):
     else:
         box_models.label(text="No ribbon folder selected", icon='INFO')
 
-    # generate textures
-    box_models_row_3 = box_models.row(align=True)
-    box_models_row_3.operator("forza.generate_textures", icon="CUBE")
-
-    is_track_folder_null_or_empty = context.scene.forza_last_track_folder is None or context.scene.forza_last_track_folder == ''
-    if is_track_folder_null_or_empty:
-        box_models_row_3.enabled = False
-    else:
-        box_models_row_3.enabled = True
+def _draw_generate_textures_button(context, layout, enabled: bool):
+    layout.separator()
+    import_row = layout.row()
+    import_row.operator("forza.generate_textures", icon="CUBE")
+    import_row.enabled = enabled
 
 def _draw_import_button(context, layout, enabled: bool):
     layout.separator()
